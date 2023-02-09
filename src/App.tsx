6cb9f259ba98edb1d31b9ea21c9ee52c643642b2
Manuel Alejandro Guerra Coello <thecoello@gmail.com>
2023-02-09 17:13:42 +0100

    *

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,43 @@ import FileUpload from "./components/fileUpload";
 import Invoice from "./components/invoice";
 import Menu from "./components/menu"
 
-export default class App extends React.Component {
+interface IProps {
+
+}
+
+
+interface IStateMenu {
+  home?:boolean,
+  invoices?:boolean,
+  filesUpload?: boolean,
+  createUser?: boolean
+}
+
+
+export default class App extends React.Component <IProps,IStateMenu> {
 
   constructor(props: any) {
     super(props)
+    this.state = {
+      home: false,
+      invoices: true,
+      filesUpload: false,
+      createUser: false
+    }
+
   }
+
+
+  showInvoices = () => {
+    this.setState({ createUser: false })
+    this.setState({ invoices: true })
+  }
+
+  showCreateUser = () => {
+    this.setState({ invoices: false })
+    this.setState({ createUser: true })
+  }
+
 
   render(): React.ReactNode {
     return (
@@ -16,14 +48,18 @@ export default class App extends React.Component {
       <div className="cotainerfluid" id="main">
         <div className="row">
           <div className="col-2">
-            <Menu />
+            <Menu
+            home={this.state.home}
+            invoices={this.showInvoices}
+            filesUpload={this.state.filesUpload}
+            createUser={this.showCreateUser()}
+            />
           </div>
           <div className="col-10" id="content-view">
             <div className="container">
 
-{/*               <CreateUser /> */}
-
-              <Invoice />
+            {this.state.invoices ? <Invoice />  : null}
+            {this.state.createUser ? <CreateUser />  : null}
             </div>
 
           </div>
