e35f932a0e45a152f6b0f3a4838c897e7201fc24
Manuel Alejandro Guerra Coello <thecoello@gmail.com>
2023-02-23 17:52:37 +0100

    Implementación de cambios para la creación de usuarios

--- src/components/users.tsx
+++ src/components/users.tsx
@@ -1,39 +1,40 @@
 import axios from "axios";
 import React from "react";
+import FormData from "form-data";
 
-interface IProps {
-
-}
+interface IProps {}
 
 interface IState {
-  priceInputDisabled?: boolean,
-  name?: string
-  contact?: string
-  email?: string
-  sponsorCategory?: string
-  location?: string
-  priceType?: string
-  price?: string
-  url?: string
-  data?: any
-  createEditUser?: boolean
-  createUser?: boolean
-  updateUser?: boolean
-  password?:string
-  userType?:string
+  priceInputDisabled?: boolean;
+  name?: string;
+  contact?: string;
+  email?: string;
+  sponsorCategory?: string;
+  location?: string;
+  priceType?: string;
+  price?: string;
+  url?: string;
+  data?: any;
+  createEditUser?: boolean;
+  createUser?: boolean;
+  updateUser?: boolean;
+  password?: string;
+  userType?: string;
 }
 
-export default class createAndUpdateUser extends React.Component<IProps, IState> {
-
+export default class createAndUpdateUser extends React.Component<
+  IProps,
+  IState
+> {
   constructor(props: IProps) {
-    super(props)
+    super(props);
     this.state = {
       priceInputDisabled: true,
       name: "",
       contact: "",
       email: "",
-      password:"",
-      userType:undefined,
+      password: "",
+      userType: undefined,
       sponsorCategory: "",
       location: "",
       priceType: "",
@@ -42,106 +43,179 @@ export default class createAndUpdateUser extends React.Component<IProps, IState>
       data: [],
       createEditUser: false,
       createUser: false,
-      updateUser: false
-    }
+      updateUser: false,
+    };
   }
 
   componentDidMount(): void {
-    axios.get(this.state.url + "/api/getusers")
-      .then((response) => {
-        this.setState({ data: response.data })
-      })
+    axios.get(this.state.url + "/api/getusers").then((response) => {
+      this.setState({ data: response.data });
+    });
   }
+
+  createUser = () => {
+
+    if(this.state.name && this.state.contact && this.state.email && this.state.password && this.state.userType ){
+      var data = new FormData();
+
+      data.append("name", this.state.name);
+      data.append("contact", this.state.contact);
+      data.append("email", this.state.email);
+      data.append("password", this.state.password);
+      data.append("user_type", this.state.userType);
+
+      var config = {
+        method: 'post',
+      maxBodyLength: Infinity,
+        url: this.state.url+'/api/postuser',
+        headers: { 
+          ...data.getHeaders()
+        },
+        data : data
+      };
+    }
+
+ 
+  };
 
   listUsers = () => {
-
-    const listUsersDiv: JSX.Element[] = []
+    const listUsersDiv: JSX.Element[] = [];
 
     this.state.data.forEach((user: any, i: number) => {
-      listUsersDiv.push(<div key={i} className="row list-item" >
-        <div className="col-4">{user.name}</div>
-        <div className="col-4">{user.email}</div>
-        <div className="col-4 text-right" id="actions">
-          <button className="btn btn-dark" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasBottom" aria-controls="offcanvasBottom" onClick={() => {
-            this.setState({ updateUser: true })
-            this.setState({ name: user.name })
-            this.setState({ contact: user.contact })
-            this.setState({ email: user.email })
-            this.setState({ password: ""})
-            this.setState({ userType: user.user_type.toString()})
-            this.setState({ createEditUser: true })
-          }}>Edit user</button>
+      listUsersDiv.push(
+        <div key={i} className="row list-item">
+          <div className="col-4">{user.name}</div>
+          <div className="col-4">{user.email}</div>
+          <div className="col-4 text-right" id="actions">
+            <button
+              className="btn btn-dark"
+              type="button"
+              data-bs-toggle="offcanvas"
+              data-bs-target="#offcanvasBottom"
+              aria-controls="offcanvasBottom"
+              onClick={() => {
+                this.setState({ updateUser: true });
+                this.setState({ name: user.name });
+                this.setState({ contact: user.contact });
+                this.setState({ email: user.email });
+                this.setState({ password: "" });
+                this.setState({ userType: user.user_type.toString() });
+                this.setState({ createEditUser: true });
+              }}
+            >
+              Edit user
+            </button>
+          </div>
         </div>
-      </div>)
+      );
     });
 
-    return <>{listUsersDiv}</>
-
-  }
+    return <>{listUsersDiv}</>;
+  };
 
   createAndUpdateUser = () => {
     return (
-      <form >
+      <form>
         <br />
 
         <div className="container">
           <div className="row">
-            <div className="col-3"> <div className="mb-3">
-              <label className="form-label">Name*</label>
-              <input value={this.state.name} onChange={(e) => {
-                this.setState({ name: e.target.value })
-              }} type="text" className="form-control" />
+            <div className="col-3">
+              {" "}
+              <div className="mb-3">
+                <label className="form-label">Name*</label>
+                <input
+                  value={this.state.name}
+                  onChange={(e) => {
+                    this.setState({ name: e.target.value });
+                  }}
+                  type="text"
+                  className="form-control"
+                />
+              </div>
             </div>
-            </div>
-            <div className="col-2"> <div className="mb-3">
-              <label className="form-label">Contact*</label>
-              <input value={this.state.contact} onChange={(e) => {
-                this.setState({ contact: e.target.value })
-              }} type="text" className="form-control" />
-            </div>
+            <div className="col-2">
+              {" "}
+              <div className="mb-3">
+                <label className="form-label">Contact*</label>
+                <input
+                  value={this.state.contact}
+                  onChange={(e) => {
+                    this.setState({ contact: e.target.value });
+                  }}
+                  type="text"
+                  className="form-control"
+                />
+              </div>
             </div>
 
-            <div className="col-3">  <div className="mb-3">
-              <label className="form-label">Email*</label>
-              <input value={this.state.email} onChange={(e) => {
-                this.setState({ email: e.target.value })
-              }} type="text" className="form-control" />
+            <div className="col-3">
+              {" "}
+              <div className="mb-3">
+                <label className="form-label">Email*</label>
+                <input
+                  value={this.state.email}
+                  onChange={(e) => {
+                    this.setState({ email: e.target.value });
+                  }}
+                  type="text"
+                  className="form-control"
+                />
+              </div>
             </div>
+            <div className="col-2">
+              {" "}
+              <div className="mb-3">
+                <label className="form-label">Password*</label>
+                <input
+                  value={this.state.password}
+                  onChange={(e) => {
+                    this.setState({ password: e.target.value });
+                  }}
+                  type="text"
+                  className="form-control"
+                />
+              </div>
             </div>
-            <div className="col-2"> <div className="mb-3">
-              <label className="form-label">Password*</label>
-              <input value={this.state.name} onChange={(e) => {
-                this.setState({ name: e.target.value })
-              }} type="text" className="form-control" />
-            </div>
-
-            </div>
-            <div className="col-2"> <div className="mb-3">
-              <label className="form-label">User Type*</label>
-              <select onChange={(e)=>{
-                this.setState({userType: e.target.value})
-              }} value={this.state.userType == "0" || this.state.userType == "1" ? this.state.userType : "DEFAULT"} className="form-select" id="floatingSelectDisabled" aria-label="Floating label disabled select example">
-                <option value="DEFAULT">Select a option</option>
-                <option value="0">User</option>
-                <option value="1">Admin</option>
-
-              </select>
-            </div>
+            <div className="col-2">
+              {" "}
+              <div className="mb-3">
+                <label className="form-label">User Type*</label>
+                <select
+                  onChange={(e) => {
+                    this.setState({ userType: e.target.value });
+                  }}
+                  value={
+                    this.state.userType == "0" || this.state.userType == "1"
+                      ? this.state.userType
+                      : "DEFAULT"
+                  }
+                  className="form-select"
+                  id="floatingSelectDisabled"
+                  aria-label="Floating label disabled select example"
+                >
+                  <option value="DEFAULT">Select a option</option>
+                  <option value="0">User</option>
+                  <option value="1">Admin</option>
+                </select>
+              </div>
             </div>
           </div>
-
         </div>
-
 
         <hr />
 
         <div className="container">
           <div className="row">
-
             <div className="col">
               <div className="mb-3">
                 <label className="form-label">Sponsorship category*</label>
-                <select defaultValue={'DEFAULT'} className="form-select" id="floatingSelectDisabled" aria-label="Floating label disabled select example">
+                <select
+                  defaultValue={"DEFAULT"}
+                  className="form-select"
+                  id="floatingSelectDisabled"
+                  aria-label="Floating label disabled select example"
+                >
                   <option value="DEFAULT">Select a option</option>
                   <option value="1">Gold Sponsor</option>
                   <option value="2">Silver Sponsor</option>
@@ -154,12 +228,19 @@ export default class createAndUpdateUser extends React.Component<IProps, IState>
             <div className="col">
               <div className="mb-3">
                 <label className="form-label">Location*</label>
-                <select defaultValue={'DEFAULT'} className="form-select" id="floatingSelectDisabled" aria-label="Floating label disabled select example">
+                <select
+                  defaultValue={"DEFAULT"}
+                  className="form-select"
+                  id="floatingSelectDisabled"
+                  aria-label="Floating label disabled select example"
+                >
                   <option value="DEFAULT">Select a option</option>
                   <option value="1">Panama (NA + LAC)</option>
                   <option value="2">Bangkok (APJ + GC)</option>
                   <option value="3">Vienna (EMEA + MEE)</option>
-                  <option value="3">Panama (NA + LAC) + Bangkok (APJ + GC) + Vienna (EMEA + MEE)</option>
+                  <option value="3">
+                    Panama (NA + LAC) + Bangkok (APJ + GC) + Vienna (EMEA + MEE)
+                  </option>
                 </select>
               </div>
             </div>
@@ -167,7 +248,12 @@ export default class createAndUpdateUser extends React.Component<IProps, IState>
             <div className="col">
               <div className="mb-3">
                 <label className="form-label">Price type*</label>
-                <select defaultValue={'DEFAULT'} className="form-select" id="floatingSelectDisabled" aria-label="Floating label disabled select example">
+                <select
+                  defaultValue={"DEFAULT"}
+                  className="form-select"
+                  id="floatingSelectDisabled"
+                  aria-label="Floating label disabled select example"
+                >
                   <option value="DEFAULT">Select a option</option>
                   <option value="1">Regular</option>
                   <option value="2">Early bird</option>
@@ -175,15 +261,21 @@ export default class createAndUpdateUser extends React.Component<IProps, IState>
               </div>
             </div>
 
-            <div className="col">  <div className="mb-3">
-              <label className="form-label">Price</label>
-              <input type="text" className="form-control" value={this.state.price} onChange={(e) => {
-                this.setState({
-                  price: e.target.value
-                })
-              }} />
-            </div>
-
+            <div className="col">
+              {" "}
+              <div className="mb-3">
+                <label className="form-label">Price</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  value={this.state.price}
+                  onChange={(e) => {
+                    this.setState({
+                      price: e.target.value,
+                    });
+                  }}
+                />
+              </div>
             </div>
           </div>
 
@@ -194,26 +286,27 @@ export default class createAndUpdateUser extends React.Component<IProps, IState>
               <div className="col-2">
                 <div className="mb-3">
                   <div className="d-flex flex-column text-center ">
-                    {this.state.createUser ? <button onClick={() => {
-                    }} className="btn btn-success" >Create User</button> : null}
+                    {this.state.createUser ? (
+                      <button onClick={() => {}} className="btn btn-success">
+                        Create User
+                      </button>
+                    ) : null}
 
-                    {this.state.updateUser ? <button onClick={() => {
-                    }} className="btn btn-success" >Update User</button> : null}
-
-
+                    {this.state.updateUser ? (
+                      <button onClick={() => {}} className="btn btn-success">
+                        Update User
+                      </button>
+                    ) : null}
                   </div>
                 </div>
               </div>
               <div className="col-10"></div>
             </div>
           </div>
-
         </div>
-
-
       </form>
-    )
-  }
+    );
+  };
 
   render(): React.ReactNode {
     return (
@@ -222,30 +315,41 @@ export default class createAndUpdateUser extends React.Component<IProps, IState>
           <div id="createAndUpdateUser">
             <div className="container">
               <div className="row">
-                <div className="col-6">  <h3>Users</h3></div>
+                <div className="col-6">
+                  {" "}
+                  <h3>Users</h3>
+                </div>
                 <div className="col-6 text-end">
-                  <button className="btn btn-dark" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasBottom" aria-controls="offcanvasBottom" onClick={() => {
-                    this.setState({ createUser: true })
-                    this.setState({ createEditUser: true })
-                    this.setState({ name: '' })
-                    this.setState({ contact: '' })
-                    this.setState({ email: '' })
-                    this.setState({ password: '' })
-                    this.setState({ userType: '' })
-
-
-                  }}>Create new user</button>
+                  <button
+                    className="btn btn-dark"
+                    type="button"
+                    data-bs-toggle="offcanvas"
+                    data-bs-target="#offcanvasBottom"
+                    aria-controls="offcanvasBottom"
+                    onClick={() => {
+                      this.setState({ createUser: true });
+                      this.setState({ createEditUser: true });
+                      this.setState({ name: "" });
+                      this.setState({ contact: "" });
+                      this.setState({ email: "" });
+                      this.setState({ password: "" });
+                      this.setState({ userType: "" });
+                    }}
+                  >
+                    Create new user
+                  </button>
                 </div>
               </div>
 
               <hr />
 
               <div className="container container-lists">
-
                 <div className="row">
                   <div className="col-4">Usuario</div>
                   <div className="col-4">Email</div>
-                  <div className="col-4 text-right" id="actions">Actions</div>
+                  <div className="col-4 text-right" id="actions">
+                    Actions
+                  </div>
                 </div>
               </div>
 
@@ -256,27 +360,38 @@ export default class createAndUpdateUser extends React.Component<IProps, IState>
               </div>
             </div>
 
-            <div className="offcanvas offcanvas-bottom h-50" id="offcanvasBottom" aria-labelledby="offcanvasBottomLabel">
-              <div className="offcanvas-header bg-black text-white
-">
-                <h3 className="offcanvas-title" id="offcanvasBottomLabel">{this.state.createUser ? "New User" : "Update User"}</h3>
-                <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close" onClick={() => {
-                  this.setState({ updateUser: false })
-                  this.setState({ createUser: false })
-                }}></button>
+            <div
+              className="offcanvas offcanvas-bottom h-50"
+              id="offcanvasBottom"
+              aria-labelledby="offcanvasBottomLabel"
+            >
+              <div
+                className="offcanvas-header bg-black text-white
+"
+              >
+                <h3 className="offcanvas-title" id="offcanvasBottomLabel">
+                  {this.state.createUser ? "New User" : "Update User"}
+                </h3>
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="offcanvas"
+                  aria-label="Close"
+                  onClick={() => {
+                    this.setState({ updateUser: false });
+                    this.setState({ createUser: false });
+                  }}
+                ></button>
               </div>
               <div className="d-flex flex-column justify-content-center container">
-
-                {this.state.createEditUser ? <this.createAndUpdateUser /> : null}
-
+                {this.state.createEditUser ? (
+                  <this.createAndUpdateUser />
+                ) : null}
               </div>
             </div>
           </div>
         </div>
-
       </>
-    )
+    );
   }
-
-
 }
